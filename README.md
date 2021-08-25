bfd9fbc56ddd9ab0b93db7647ceb63ae5a798a81
root <e.abiynur@gmail.com>
2021-08-25 11:54:20 -0700

    shell

---
+++ README.md
@@ -0,0 +1,13 @@
+Simple Shell.c
+
+Concepts:
+ write a simple UNIx command interpreter
+Usage:
+ -type "./hsh" to enter shell
+ -prompted with the shell inerface
+ -type command and press "Enter"
+ -output
+ -prompt
+ -exit type "exit" or "Ctrl D"
+Output:
+ it must have the exact same output as sh(/bin/sh) as well as the exact same error output
---
+++ simple_shell.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+#define delim " \n"
+
+/**
+ * main -
+ * @prompt: to write a command
+ * @split: splits the command into argument
+ * @check: checks if the command is excutable
+ * @execute: executes the command
+ * @loop: loops
+ * Return: 0
+ */
+
+char *prompt()
+{
+    char *cmnd;
+    size_t cmndsize = 128;
+    size_t cxcs;
+
+    cmnd = (char *)malloc(128 * sizeof(char));
+    printf("$ ");
+    cxcs = getline(&cmnd, &cmndsize, stdin);
+   if (feof(stdin))
+   {
+       exit(-1);
+   }
+
+    return (cmnd);
+}
+
+
+char **split(char *str)
+{
+    char *token;
+    token = strtok(str, delim);
+    char **total;
+    total = malloc(64 * sizeof(char *));
+    int i;
+    i = 0;
+
+    while (token != NULL)
+    {
+	    total[i] = token;
+	    i++;
+	    token = strtok(NULL, delim);
+    }
+    total[i] = NULL;
+    return (total);
+}
+
+
+int execute(char **argu)
+{
+    pid_t pid;
+    if (access(argu[0], X_OK))
+    {
+	    perror("file not found");
+	    return (1);
+    }
+    pid = fork();
+    if (pid == 0)
+    {
+	    execve(argu[0], argu, NULL);
+	    return (-1);
+    }
+    else
+    {
+	    waitpid(pid, NULL, WUNTRACED);
+    }
+    return (0);
+}
+
+void loop(void)
+{
+    int status;
+    char *cmd;
+    char **args;
+    do {
+	    cmd = prompt();
+	    args = split(cmd);
+	    status = execute(args);
+    } while (status != -1);
+    return;
+}
+int main(int argc, char const *argv[])
+{
+    loop();
+    return (0);
+}

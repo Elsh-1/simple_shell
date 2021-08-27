61a57e813fe3c9eb8a9d5e4d3de4088f226279bc
root <e.abiynur@gmail.com>
2021-08-26 20:01:15 -0700

    shelll

--- simple_shell1.c
+++ simple_shell1.c
@@ -20,75 +20,75 @@
 
 char *prompt()
 {
-    char *cmnd;
-    size_t cmndsize = 128;
-    cmnd = (char *)malloc(128 * sizeof(char));
-    printf("$ ");
-    getline(&cmnd, &cmndsize, stdin);
-   if (feof(stdin))
-   {
-       exit(-1);
-   }
-
-    return (cmnd);
+	char *cmnd;
+	size_t cmndsize = 128;
+	cmnd = (char *)malloc(128 * sizeof(char));
+	printf("$ ");
+	getline(&cmnd, &cmndsize, stdin);
+	if (feof(stdin))
+	{
+		exit(-1);
+	}
+	
+	return (cmnd);
 }
 
 
 char **split(char *str)
 {
-    char *token;
-    char **total;
-    int i;
-    token = strtok(str, delim);
-    total = malloc(64 * sizeof(char *));
-    i = 0;
-
-    while (token != NULL)
-    {
-	    total[i] = token;
-	    i++;
-	    token = strtok(NULL, delim);
-    }
-    total[i] = NULL;
-    return (total);
+	char *token;
+	char **total;
+	int i;
+	token = strtok(str, delim);
+	total = malloc(64 * sizeof(char *));
+	i = 0;
+	
+	while (token != NULL)
+	{
+		total[i] = token;
+		i++;
+		token = strtok(NULL, delim);
+	}
+	total[i] = NULL;
+	return (total);
 }
 
 
 int execute(char **argu)
 {
-    pid_t pid;
-    if (access(argu[0], X_OK))
-    {
-	    perror("file not found");
-	    return (1);
-    }
-    pid = fork();
-    if (pid == 0)
-    {
-	    execve(argu[0], argu, NULL);
-	    return (-1);
-    }
-    else
-    {
-	    waitpid(pid, NULL, WUNTRACED);
-    }
-    return (0);
+	pid_t pid;
+	if (access(argu[0], X_OK))
+	{
+		perror("file not found");
+		return (1);
+	}
+	pid = fork();
+	if (pid == 0)
+	{
+		execve(argu[0], argu, NULL);
+		return (-1);
+	}
+	else
+	{
+		waitpid(pid, NULL, WUNTRACED);
+	}
+	return (0);
 }
 
 void loop(void)
 {
-    int status;
-    char *cmd;
-    char **args;
-    do {
-	    cmd = prompt();
-	    args = split(cmd);
-	    status = execute(args);
-    } while (status != -1);
-    return;
+	int status;
+	char *cmd;
+	char **args;
+	do {
+		cmd = prompt();
+		args = split(cmd);
+		status = execute(args);
+       	} while (status != -1);
+	return;
 }
 int main(void)
 {
-    loop();
-    return (0);
+	loop();
+	return (0);
 }
